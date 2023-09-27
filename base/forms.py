3ac3ad95aac84ee958ae5dd7b57434b54ba7fa25
mdnaimur <mdnaimurr@gmail.com>
2023-09-27 15:21:43 +0600

    Emloyees function module add

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm, fields
 
-from .models import User
+from .models import User, Product_Assets, Employee
 
 # ? Custom User from class making
 
@@ -10,3 +10,16 @@ class MyUserFrom(UserCreationForm):
     class Meta:
         model = User
         fields = ['name', 'username', 'email', 'password1', 'password2']
+
+
+class Poducts_Form(ModelForm):
+    class Meta:
+        model = Product_Assets
+        fields = '__all__'
+
+
+class Employee_Form(ModelForm):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+        exclude = ['host']
